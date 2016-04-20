7728dd1cf4cd8395edd681ef6759c20b26b3c3c6
Bruno Casella <bcasella@localhost.localdomain>
2016-04-19 23:44:46 -0300

    Initial eve example

---
+++ people.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+from eve import Eve
+app = Eve()
+
+if __name__ == '__main__':
+    app.run()
---
+++ settings.py
@@ -0,0 +1,103 @@
+schema = {
+    # Schema definition, based on Cerberus grammar. Check the Cerberus project
+    # (https://github.com/nicolaiarocci/cerberus) for details.
+    'object': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 30,
+        'required' : True,
+    },
+    'permission': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 35,
+        'required': True,
+
+    },
+    # 'role' is a list, and can only contain values from 'allowed'.
+}
+
+schema_acc =  {
+    'username': {
+        'type': 'string',
+        'required': True,
+        'unique': True,
+        },
+    'password': {
+        'type': 'string',
+        'required': True,
+    },
+    'roles': {
+             'type': 'list',
+             'allowed': ['user', 'superuser', 'admin'],
+             'required': True,
+         }
+
+}
+
+
+accounts = {
+    # the standard account entry point is defined as
+    # '/accounts/<ObjectId>'. We define  an additional read-only entry
+    # point accessible at '/accounts/<username>'.
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'username',
+    },
+
+    # We also disable endpoint caching as we don't want client apps to
+    # cache account data.
+    'cache_control': '',
+    'cache_expires': 0,
+
+    # Finally, let's add the schema definition for this endpoint.
+    'schema': schema_acc,
+}
+
+
+
+
+permission = {
+    # 'title' tag used in item links. Defaults to the resource title minus
+    # the final, plural 's' (works fine in most cases but not for 'people')
+    'item_title': 'permissions',
+
+    # by default the standard item entry point is defined as
+    # '/people/<ObjectId>'. We leave it untouched, and we also enable an
+    # additional read-only entry point. This way consumers can also perform
+    # GET requests at '/people/<lastname>'.
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'object'
+    },
+
+    # We choose to override global cache-control directives for this resource.
+    'cache_control': 'max-age=10,must-revalidate',
+    'cache_expires': 10,
+
+    # most global settings can be overridden at resource level
+    'resource_methods': ['GET', 'POST', 'DELETE'],
+
+    'schema': schema
+}
+
+DOMAIN = {'permissions': permission,
+    'accounts' : accounts}
+
+
+# Let's just use the local mongod instance. Edit as needed.
+
+# Please note that MONGO_HOST and MONGO_PORT could very well be left
+# out as they already default to a bare bones local 'mongod' instance.
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_DBNAME = 'apitest'
+
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+# Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
+# individual items  (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
